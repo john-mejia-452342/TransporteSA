a4de9eedc969a5fbbab6db391fea6c7e8472b95f
unknown <SENA@SGLDFPCATSD254.FormacionSena.RED>
2023-10-17 14:03:17 -0500

    Funcionando

---
+++ src/components/Login.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <q-btn color="secondary"><router-link to="/buses">Buses</router-link></q-btn>
+    </div>
+  </template>
+  
+  <script setup>
+  
+  </script>
+  
+  <style scoped>
+  
+  
+  </style>
+  
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,12 +1,24 @@
-import Buses from "../components/Buses.vue"
-import Main from "../components/Main.vue"
+import Login from "../components/Login.vue"
+import Menu from "../components/Menu.vue"
 
 
 import {createRouter, createWebHashHistory} from 'vue-router'
 
 const routes = [
-    {path: "/", component:Main},
-    {path: "/buses", component:Buses}
+    {path: "/",component: Login,},
+    {path: "/menu",component: Menu,
+    //   children: [
+    //     { path: "", redirect: "/menu" },
+    //     { path: "/infoEmpresa", component: infoEmpresa },
+    //     { path: "/clientes", component: clientes },
+    //     { path: "/conductores", component: conductores },
+    //     {path : "/revision" , component : revision},
+    //     {path : "/rutas" , component :rutas},
+    //     {path : "/ticket" , name:"Ticket", component :ticket},
+    //     {path : "/vehiculos" , component : vehiculos},
+    //     {path : "/venta" , component : ventas}
+    //   ],
+    }
 ]
 
 export const router = createRouter({
