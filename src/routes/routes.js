7325a56d7c83bd42e3f743aaa030cd9aae85dda4
unknown <SENA@SGLDFPCATSD254.FormacionSena.RED>
2023-10-17 14:37:32 -0500

    Funcionandooo

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,6 @@
 import Login from "../components/Login.vue"
 import Menu from "../components/Menu.vue"
-import Vendedor from "../components/Vendedor.vue"
+// import Vendedor from "../components/Vendedor.vue"
 
 
 import {createRouter, createWebHashHistory} from 'vue-router'
@@ -8,10 +8,10 @@ import {createRouter, createWebHashHistory} from 'vue-router'
 const routes = [
     {path: "/",component: Login,},
     {path: "/menu",component: Menu,
-      children: [
-        { path: "", redirect: "/menu" },
-        { path: "/vendedor", component: Vendedor },
-      ],
+    //   children: [
+    //     { path: "", redirect: "/menu" },
+    //     { path: "/vendedor", component: Vendedor },
+    //   ],
     }
 ]
 
